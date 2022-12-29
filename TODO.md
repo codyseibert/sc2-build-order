7af777e68918b10f0cc4aad18d5224583bb8cb7d
Cody Seibert <cseibert113@gmail.com>
2022-12-28 21:14:56 -0500

    initial setup with build order model

---
+++ TODO.md
@@ -0,0 +1,2 @@
+- as a user, I shall be able to upload a sc2 build order text snippet
+- as a user, I shall be able to view build orders based on match up
---
+++ prisma/migrations/20221229021419_adding_build_order_model/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "BuildOrder" (
+    "id" TEXT NOT NULL,
+    "matchUp" TEXT NOT NULL,
+    "build" TEXT NOT NULL,
+
+    CONSTRAINT "BuildOrder_pkey" PRIMARY KEY ("id")
+);
